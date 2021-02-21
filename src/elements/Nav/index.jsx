ea948dcacb452b72ecc06147341cca44461141af
Enrique Mejia <enriquemejiacano@gmail.com>
2021-02-21 18:39:44 -0500

    updating responsive menu

--- src/elements/Nav/index.jsx
+++ src/elements/Nav/index.jsx
@@ -11,11 +11,27 @@ const Nav = ({options, color}) => {
             </div>
             <div className="hidden md:block">
               <div className="ml-10 space-x-6">
-                {options?.map((item, index) => <a key={index} href="#" className="item-menu">{item}</a> 
-                )}
+                {options?.map((item, index) => <a key={index} href="#" className="item-menu">{item}</a>)}
               </div>
             </div>
+            <div className="-mr-2 flex md:hidden">
+              <button type="button" className="bg-yellow-700 inline-flex items-center justify-center p-2 rounded-md text-yellow-200 hover:text-white hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-yellow-700 focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                <span className="sr-only">Open main menu</span>
+                <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+                <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+            </div>          
           </div>          
+      </div>
+
+      <div className="md:hidden" id="mobile-menu">
+        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+          {options?.map((item, index) => <a key={index} href="#" className="item-menu block">{item}</a>)} 
+        </div>
       </div>
     </nav>
   )
